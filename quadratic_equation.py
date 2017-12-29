b63732d91cc89de44f9951b0ba723f2b8b41cc55
Swarrt <swarrt@gmail.com>
2017-12-29 11:37:02 +0300

    исправлен баг с корнями квадратного уравнения при отрицательном дискриминанте

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,9 +3,14 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    try:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
+    except ValueError:
+        pass
     if discriminant == 0:
         return root1, None
+    elif discriminant < 0:
+        return None, None
     else:
         return root1, root2
